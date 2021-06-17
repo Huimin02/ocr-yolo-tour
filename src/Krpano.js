b0353bdbf45ea10d946a7b3b07b047fa31009418
Min <huimin@pitrees.com>
2021-06-17 18:49:25 +0800

    Bottom Bar + Sales Gallery

--- src/Krpano.js
+++ src/Krpano.js
@@ -1,10 +1,26 @@
-import React, {useEffect} from 'react'
+import React, {useState} from 'react'
 import classNames from 'classnames'
 import useDemoKrpano from './hooks/useDemoKrpano'
 import './asset/demo-page.css'
 import { Box,Center, Button, HStack, VStack, Stack, Text, Image, } from '@chakra-ui/react'
 import malaysiaOff from './asset/button/01_Malaysia_1.png'
 import malaysiaOn from './asset/button/01_Malaysia_2.png'
+import languageOff from './asset/button/00_Language_1.png'
+import languageOn from './asset/button/00_Language_2.png'
+import yoloOff from './asset/button/03_Logo_2.png'
+import yoloOn from './asset/button/03_Logo_1.png'
+import showUnitOff from './asset/button/04_ShowUnit_1.png'
+import showUnitOn from './asset/button/04_ShowUnit_2.png'
+import salesGalleryOff from './asset/button/05_SalesGallery_1.png'
+import salesGalleryOn from './asset/button/05_SalesGallery_2.png'
+import eBrochureOff from './asset/button/06_Ebrochure_1.png'
+import eBrochureOn from './asset/button/06_Ebrochure_2.png'
+import videoOff from './asset/button/07_Video_1.png'
+import videoOn from './asset/button/07_Video_2.png'
+import contextViewOff from './asset/button/08_ContextView_1.png'
+import contextViewOn from './asset/button/08_ContextView_2.png'
+import ocr from './asset/button/OCR_LOGO.png'
+
 
 const LoadingPage = ({ isFadingout = false }) => {
   const wrapperClasses = classNames('loading-page', {
@@ -32,17 +48,15 @@ const Krpano = () => {
     callKrpano
   } = useDemoKrpano()
 
+  const [showMsiaMap, setShowMsiaMap] = useState(false)
 
-  useEffect(() => {
+  // function goToPano() {
     
-  }, [])
+  //   // var krpano = document.getElementById("krpanoSWFObject");
+  //   // callKrpano("loadpano(" + 'aerial.xml' + ", null, MERGE, BLEND(1));");
+  //   callKrpano("loadscene('scene_arial_(rgb)-01',null,MERGE,ZOOMBLEND(1,2,'easeInOutSine'));");
+  // }
 
-  function goToPano() {
-    
-    // var krpano = document.getElementById("krpanoSWFObject");
-    // callKrpano("loadpano(" + 'aerial.xml' + ", null, MERGE, BLEND(1));");
-    callKrpano("loadscene('scene_arial_(rgb)-01',null,MERGE,ZOOMBLEND(1,2,'easeInOutSine'));");
-  }
 
   return (
     <>
@@ -52,12 +66,12 @@ const Krpano = () => {
 
       <Box h="100vh" overflowX="hidden" bg="#FFD64E">
         <Box w="100%" ref={containerRef} />
-        <HStack align="center" justify="center" bg="#FFD64E" h="8vh" w="100%" bottom="0" position="absolute" zIndex="2" >
+        <HStack align="center" justify="center" bg="#FFD64E" h="7vh" w="100%" bottom="0" position="absolute" zIndex="2" >
 
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image  draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image w="55%" _hover={{ src: {languageOn}}} draggable={false} src={languageOff}/>
+
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
@@ -66,28 +80,36 @@ const Krpano = () => {
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image draggable={false} w="60%" src={yoloOff} onClick= {() => callKrpano("loadscene('scene_arial_(rgb)-01',null,MERGE,ZOOMBLEND(1,2,'easeInOutSine'));")}/>
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image draggable={false} w="60%" src={contextViewOff} onClick={() =>  callKrpano("loadscene('scene_in_to_out_-_40_level-min',null,MERGE,ZOOMBLEND(1,2,'easeInOutSine'));")}/>
+            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Context View</Text>
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image draggable={false} w="60%" src={showUnitOff}/>
+            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Show Unit</Text>
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image draggable={false} w="60%" src={salesGalleryOff} onClick={() => callKrpano("loadscene('scene_Gallery_A',null,MERGE,ZOOMBLEND(1,2,'easeInOutSine'));")}/>
+            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Sales Gallery</Text>
           </Center>
 
           <Center cursor="pointer" flexDir="column" w="15%" >
-            <Image draggable={false} w="60%" src={malaysiaOff}/>
-            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Malaysia</Text>
+            <Image draggable={false} w="60%" src={eBrochureOff}/>
+            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">E-Brochure</Text>
+          </Center>
+
+          <Center cursor="pointer" flexDir="column" w="15%" >
+            <Image draggable={false} w="60%" src={videoOff}/>
+            <Text fontSize={["0.6rem","0.6rem","1rem","1rem"]}  mt="10" pos="absolute">Video</Text>
+          </Center>
+
+          <Center cursor="pointer" flexDir="column" w="15%" >
+            <Image draggable={false} w="60%" src={ocr}/>
           </Center>
 
       
